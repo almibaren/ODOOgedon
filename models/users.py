25a3f551a7bc22b966c92c69eaec9fdc2f4f0479
almibaren <almibaren@gmail.com>
2019-01-17 12:20:49 +0100

    Acualizado modelo User para comunicarse con hats + Menu lateral

--- models/users.py
+++ models/users.py
@@ -5,9 +5,9 @@ class User(models.Model):
     _description = 'Users for Worms'
     _order = 'name'
 
-    name = fields.Char('Username', required=True)
-    password = fields.Char('Password', required=True)
     realName = fields.Char('Name')
     surname = fields.Char('Surname')
+    name = fields.Char('Username', required=True)
+    password = fields.Char('Password', required=True)
     email = fields.Char('Email', required=True)
-    username = fields.Char('Username', required=True)
+    hats = fields.Many2many('almibaren.worms.hats',string='Gorros')
