46fc367b97ace6ee721902978f5bd737170bb50e
almibaren <almibaren@gmail.com>
2019-01-22 13:26:06 +0100

    Grafico terminado

--- models/hats.py
+++ models/hats.py
@@ -12,3 +12,16 @@ class Hats(models.Model):
     prize = fields.Float('Precio',(3,1))
     cover = fields.Many2many('almibaren.worms.images',String="Imagen")
     rating = fields.Float('Valoración', (3, 1))
+    sales = fields.Integer(String='Total ventas',compute='_get_total_sales2',store=True)
+    #sql = fields.Char(String="SQL", compute = '_get_consulta')
+    user_ids = fields.Many2many('almibaren.worms.user', string="Usuario", relation="almibaren_worms_hats_almibaren_worms_user_rel")
+
+    @api.model
+    def _get_total_sales(self):
+        self.sales = self._cr.execute("SELECT COUNT(almibaren_worms_hats.name) FROM public.almibaren_worms_hats_almibaren_worms_user_rel, public.almibaren_worms_hats WHERE almibaren_worms_hats_almibaren_worms_user_rel.almibaren_worms_hats_id = almibaren_worms_hats.id AND almibaren_worms_hats.name='" + self.name +"'");
+        return self.sales;
+
+    @api.depends('user_ids')
+    def _get_total_sales2(self):
+        for r in self:
+            r.sales = len(r.user_ids)
